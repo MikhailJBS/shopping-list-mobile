ec83aa8c772b4f1331808bacbbcd18f0f320867f
MikhailJBS <mikhailharitz@gmail.com>
2023-11-08 15:47:24 +0700

    Finished Tutorial 7

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/widgets/left_drawer.dart';
+import 'package:shopping_list/widgets/shoplist_form.dart';
 
 class MyHomePage extends StatelessWidget {
     MyHomePage({Key? key}) : super(key: key);
@@ -10,13 +12,17 @@ class MyHomePage extends StatelessWidget {
     ];
 
     @override
-    Widget build(BuildContext context) {
-        return Scaffold(
+    Widget build(BuildContext context) {  
+          return Scaffold(
             appBar: AppBar(
               title: const Text(
                 'Shopping List',
               ),
+              backgroundColor: Colors.indigo,
+              foregroundColor: Colors.white,
             ),
+            // Masukkan drawer sebagai parameter nilai drawer dari widget Scaffold
+            drawer: const LeftDrawer(),
             body: SingleChildScrollView(
               // Widget wrapper yang dapat discroll
               child: Padding(
@@ -82,6 +88,16 @@ class ShopCard extends StatelessWidget {
             ..hideCurrentSnackBar()
             ..showSnackBar(SnackBar(
                 content: Text("Kamu telah menekan tombol ${item.name}!")));
+
+          // Navigate ke route yang sesuai (tergantung jenis tombol)
+          if (item.name == "Tambah Produk") {
+            // TODO: Gunakan Navigator.push untuk melakukan navigasi ke MaterialPageRoute yang mencakup ShopFormPage.
+                if (item.name == "Tambah Produk") {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const ShopFormPage()));
+              }
+          }
+
         },
         child: Container(
           // Container untuk menyimpan Icon dan Text
